ee39ce9687dd7c36c8e2bc41ad237fefd5694d9d
Ebenezer Scrooge <1641367382@qq.com>
2020-12-15 17:10:21 +0800

    Update train.py

--- train.py
+++ train.py
@@ -108,4 +108,5 @@ def _main(args):
 if __name__ == '__main__':
     # _main(parser.parse_args())
     # _main(parser.parse_args(['30', '32', 'D:/Datasets/VOC/VOCdevkit']))
+    # _main(parser.parse_args(['1', '32', 'C:\\BaiduNetdiskDownload\\pascalvoc\\VOCdevkit']))
     _main(parser.parse_args(['1', '32', 'C:\\BaiduNetdiskDownload\\pascalvoc\\VOCdevkit']))
